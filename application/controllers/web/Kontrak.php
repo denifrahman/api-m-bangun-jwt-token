ce811c5593dcc9c6e14fbc000615501ab420c736
denifrahman <Denifa2412@>
2020-07-02 23:40:08 +0700

    test

--- application/controllers/web/Kontrak.php
+++ application/controllers/web/Kontrak.php
@@ -102,7 +102,7 @@ class Kontrak extends \Restserver\Libraries\REST_Controller
     public function pdf_get()
     {
         $produkid = $this->get('id', TRUE);
-        $this->load->library('pdf');
+        $this->load->library('Pdf');
         $data['kontrak'] = $this->Kontrak_Model->getKontrakByProdukId_Kontrak($produkid);
             // var_dump($data['kontrak']);
             // die;
