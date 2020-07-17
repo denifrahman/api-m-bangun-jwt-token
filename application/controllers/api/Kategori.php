d92e93799f169951acecad00d4b8087f4252453d
denifrahman <denifrahman@gmail.com>
2020-07-17 11:06:12 +0700

    groupByFlag

--- application/controllers/api/Kategori.php
+++ application/controllers/api/Kategori.php
@@ -124,6 +124,61 @@ class Kategori extends \Restserver\Libraries\REST_Controller
             $this->response($message, REST_Controller::HTTP_NOT_FOUND);
         }
     }
+    /**
+     * Kategori Get Data
+     * --------------------
+     * --------------------------
+     * @method : GET
+     * @link: api/Kategori/getAll
+     */
+    public function getAllGroupByParam_get()
+    {
+        $produkkategoriid = $this->get('produkkategoriid');
+        $produkkategorinama = $this->get('produkkategorinama');
+        $produkkategoriaktif = $this->get('produkkategoriaktif');
+        $produkkategoriakses = $this->get('produkkategoriakses');
+        $produkkategorireq = $this->get('produkkategorireq');
+        $produkkategoriflag = $this->get('produkkategoriflag');
+        header("Access-Control-Allow-Origin: *");
+
+        // Load Authorization Token Library
+        $this->load->library('Authorization_Token');
+
+        /**
+         * User Token Validation
+         */
+        $is_valid_token = $this->authorization_token->validateToken();
+        if (!empty($is_valid_token) and $is_valid_token['status'] === TRUE) {
+            $data =$this->Kategori_Model->getAllByParam_kategori($produkkategoriid, $produkkategorinama,  $produkkategoriakses, $produkkategorireq, $produkkategoriflag, $produkkategoriaktif);
+            $tmp = array();
+
+            foreach ($data as $arg) {
+                $tmp[$arg->produkkategoriflag][] = $arg;
+            }
+
+            $output = array();
+            
+            foreach ($tmp as $type => $labels) {
+                $output[] = array(
+                    'flag' => $type,
+                    'chilrdern'=>$labels
+                );
+            }
+            // var_dump($output);
+            // die;
+            $message = array(
+                'status' => $is_valid_token['status'],
+                'data' => $output,
+            );
+            $this->response($message, REST_Controller::HTTP_NOT_FOUND);
+        } else {
+            $message = array(
+                'status' => $is_valid_token['status'],
+                'message' => $is_valid_token['message'],
+            );
+            $this->response($message, REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
 
     /**
      * Kategori Get Data All By id 
